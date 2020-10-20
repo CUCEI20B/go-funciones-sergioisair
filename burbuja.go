2abb9dc92e7cff662d3b21a67c65dbe0782ad769
Sergio Isair <sergioisair@gmail.com>
2020-10-20 12:45:55 -0500

    PRimer prueba burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,19 @@
 package main
 
-func Burbuja(s []int64)  {
-
+func main() {
+	s := []int64{}
+	Burbuja(s)
 }
 
-func main()  {
-	
+func Burbuja(s []int64) {
+	var aux int64
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); j++ {
+			if s[i] < s[j] {
+				aux = s[i]
+				s[i] = s[j]
+				s[j] = aux
+			}
+		}
+	}
 }
